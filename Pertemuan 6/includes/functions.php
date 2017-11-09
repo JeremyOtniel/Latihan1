05c94dfaf521a2053ea651e259b47d77b47089c7
JeremyOtniel <JeremyOtniel@users.noreply.github.com>
2017-11-09 20:30:24 +0700

    Finding a subjeFinding a subject in databasect in database

--- Pertemuan 6/includes/functions.php
+++ Pertemuan 6/includes/functions.php
@@ -9,7 +9,7 @@ function find_all_subjects(){
 	global $connection;
 	$query = "SELECT * ";
 	$query .= "FROM subjects ";
-	$query .= "WHERE visible = 1 ";
+//	$query .= "WHERE visible = 1 ";
 	$query .= "ORDER by position ASC";
 	$subject_set = mysqli_query($connection, $query);
 	confirm_query($subject_set);
@@ -17,10 +17,13 @@ function find_all_subjects(){
 }
 function find_pages_for_subject($subject_id){
 	global $connection;
+
+	$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
 	$query = "SELECT * ";
 	$query .= "FROM pages ";
 	$query .= "WHERE visible = 1 ";
-	$query .= "AND subject_id= {$subject_id} ";
+	$query .= "AND subject_id= {$safe_subject_id} ";
 	$query .= "ORDER by position ASC";
 	$page_set = mysqli_query($connection, $query);
 	confirm_query($page_set);
@@ -64,5 +67,23 @@ function navigation($subject_id, $page_id){
 			mysqli_free_result($subject_set);
 			$output .= "</ul>";
 			return $output;
+}
+
+function find_subject_by_id($subject_id){
+	global $connection;
+
+	$safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
+	$query = "SELECT * ";
+	$query .= "FROM subjects ";
+	$query .= "WHERE id = {$safe_subject_id} ";
+	$query .= "LIMIT 1";
+	$subject_set = mysqli_query($connection, $query);
+	confirm_query($subject_set);
+	if ($subject = mysqli_fetch_assoc($subject_set)){
+		return $subject;
+	} else{
+	return null;
+	}	
 }
 ?>
